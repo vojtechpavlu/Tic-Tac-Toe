1b69f44fe663bad4a8c996dee8fe434781631ecb
vojtechpavlu <vojtech.pavlu.dev>
2022-11-12 23:04:23 +0100

    Add another tests for the Game
    Tests for Game creation in terms of the players is defined.

--- test/test_game.py
+++ test/test_game.py
@@ -1,4 +1,3 @@
-
 import pytest
 
 from src.game.game import Game, GameError
@@ -24,5 +23,27 @@ def test_game_creation_only_one_player():
         assert Game([DummyPlayer("SinglePlayer", "X")])
 
 
+def test_game_creation_more_than_two_players():
+    with pytest.raises(GameError) as ge:
+        assert Game([
+            DummyPlayer("Triplet1", "X"),
+            DummyPlayer("Triplet2", "X"),
+            DummyPlayer("Triplet3", "X")
+        ])
 
 
+def test_game_creation_two_players_with_same_mark():
+    with pytest.raises(GameError) as ge:
+        assert Game([DummyPlayer("A", "X"), DummyPlayer("B", "X")])
+
+
+def test_game_returns_players():
+    game = Game([DummyPlayer("A", "X"), DummyPlayer("B", "O")])
+    assert len(game.players) == 2
+    assert game.players[0].player_name == "A"
+    assert game.players[1].player_name == "B"
+
+
+def test_game_returns_player_names():
+    game = Game([DummyPlayer("A", "X"), DummyPlayer("B", "O")])
+    assert game.player_names == ("A", "B")
