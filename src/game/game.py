f88fec3faf8bf844adca79103f28977c4c692765
vojtechpavlu <vojtech.pavlu.dev>
2022-11-12 23:05:02 +0100

    Update of the game's __init__
    Now the initor of the Game was redefined to pass the defined tests

--- src/game/game.py
+++ src/game/game.py
@@ -15,6 +15,30 @@ class Game:
         self.__players = list(players)
         self.__board = board
 
+        # Pokud je počet hráčů jiný než 2
+        if len(self.players) != 2:
+            raise GameError(f"Počet hráčů musí být 2: {len(self.players)}")
+
+        # Pokud je počet unikátních značek jiný, než počet hráčů
+        elif len(set(self.player_marks)) != len(self.players):
+            raise GameError(
+                f"Každý hráč musí mít unikátní značku: {self.player_marks}")
+
+    @property
+    def players(self) -> tuple[Player]:
+        """"""
+        return tuple(self.__players)
+
+    @property
+    def player_names(self) -> tuple[str]:
+        """"""
+        return tuple([player.player_name for player in self.players])
+
+    @property
+    def player_marks(self) -> tuple[str]:
+        """"""
+        return tuple([player.mark for player in self.players])
+
 
 class GameError(Exception):
     pass
