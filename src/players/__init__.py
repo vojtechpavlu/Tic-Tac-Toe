c85281cd1170b93a57e1ddacb844d60063b368e9
vojtechpavlu <vojtech.pavlu.dev@gmail.com>
2022-11-25 23:20:54 +0100

    Add shorthand importing at the package level
    To enable easier importing of all the players, now the shorthand `__init__.py` module contains importing of each of the player classes from their own packages.

---
+++ src/players/__init__.py
@@ -0,0 +1,11 @@
+"""Balíček hráčů obsahuje definice jednotlivých tříd hráčů. Ti mají společného
+abstraktního předka `Player`, který slouží jako deklarace obecného,
+neinstaciovatelného protokolu pro všechny hráče.
+
+Tento modul má za cíl fungovat jako zkratka pro snazší a přehlednější import
+všech hráčů.
+"""
+
+from .human_player.human_player import HumanPlayer
+from .random_player.random_npc_player import RandomNPCPlayer
+from .custom_player.custom_player import CustomPlayer
