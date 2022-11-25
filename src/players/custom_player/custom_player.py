175c154d4ee722c1c320e8e8c0fb3864432f82b9
vojtechpavlu <vojtech.pavlu.dev@gmail.com>
2022-11-25 22:57:32 +0100

    Add simple docstring

--- src/players/custom_player/custom_player.py
+++ src/players/custom_player/custom_player.py
@@ -1,17 +1,20 @@
-""""""
+"""Jednoduchý modul coby přístupový bod pro vlastní implementace automatického
+hráče.
+"""
+
 from src.game.board import BoardSnapshot
 from src.game.player import Player
 
 
 class CustomPlayer(Player):
-    """"""
+    """Šablona hráče pro studenty."""
 
     def __init__(self, player_name: str, mark: str):
-        """"""
+        """Jednoduchý initor"""
         Player.__init__(self, player_name, mark)
 
     def move(self, board: BoardSnapshot, valid_moves: tuple[str]) -> str:
-        """"""
+        """Obecná metoda, která je třeba pro implementaci."""
         return valid_moves[0]
 
 
