5354dc24ec8c96e9a313f933265e4389df4e4399
vojtechpavlu <vojtech.pavlu.dev@gmail.com>
2023-02-02 17:52:10 +0100

    Docstring fulfillment

--- src/players/rational_npc_player/heuristic_minmax_npc.py
+++ src/players/rational_npc_player/heuristic_minmax_npc.py
@@ -1,4 +1,12 @@
-""""""
+"""Tento modul obsahuje definici heuristického racionálního hráče, který
+vybírá své tahy na základě pomocných evaluačních funkcí.
+
+Jeho síla oproti rigidnímu a rigoróznímu `MinmaxPlayer` je schopnost omezeného
+prohledávání do hloubky, díky čemuž sice může obětovat absolutně nejlepší tah
+na úkor zásadnímu zlepšení výkonu. Díky snížené hloubky prohledávání je pomocí
+tohoto přístupu schopen částečně překračovat problém s komplexitou vícepolních
+herních ploch.
+"""
 
 from typing import Iterable
 from src.game.board import BoardSnapshot
@@ -8,36 +16,55 @@ from src.players.rational_npc_player.utils import is_terminate, translate_board
 
 
 class LimitedMinmaxPlayer(Player):
-    """"""
+    """Instance této třídy jsou odpovědné za racionální hraní hry piškvorky,
+    přičemž uvažují jen omezený počet tahů.
 
-    def __init__(self, player_name: str, mark: str, evaluators: Iterable[Evaluator] = (), max_depth: int = 9):
+    Toho dosahují pomocí množiny evaluátorů, které dokáží vyhodnotit kvalitu
+    aktuálního snímku hrací plochy, díky čemuž může odhadovat bonitu svých
+    rozhodování v 'reálném čase'.
+    """
+
+    def __init__(self, player_name: str, mark: str,
+                 evaluators: Iterable[Evaluator] = (), max_depth: int = 9):
+        """Initor, který přijímá hráčovo jméno, značku, kterou označuje svá
+        políčka (tahy), iterovatelnou množinu evaluačních funkcí (instancí
+        typu `Evaluator`), které budou použity pro vyhodnocování tahů, a
+        hloubku maximálního prohledávání.
+
+        Pro maximální hloubku prohledávání platí, že čim vyšší, tím
+        kvalitnější tahy hráč hraje. Ovšem za cenu vyšší výpočetní složitosti,
+        kterou musí při volbě tahu překonat.
+        """
         super().__init__(player_name, mark)
         self.__max_depth = max_depth
         self.__evaluators: list[Evaluator] = list(evaluators)
 
     @property
     def max_depth(self) -> int:
-        """"""
+        """Maximální hloubka, ve které bude algoritmus prohledávat."""
         return self.__max_depth
 
     @property
     def evaluators(self) -> tuple[Evaluator]:
-        """"""
+        """Ntice evaluačních funkcí, které jsou při rozhodování použity."""
         return tuple(self.__evaluators)
 
     def add_evaluator(self, new_evaluator: Evaluator):
-        """"""
+        """Funkce pro přidání nové evaluační funkce pro ohodnocování svých
+        rozhodnutí."""
         self.__evaluators.append(new_evaluator)
 
     def move(self, board: BoardSnapshot, valid_moves: tuple[str]) -> str:
-        """"""
+        """Funkce odpovědná za výběr následujícího tahu."""
         new_board = translate_board(board)
         return self.limited_minmax(
             new_board, True, self.max_depth)[1]
 
     def limited_minmax(self, board: list[list[str]], is_maximizing: bool,
                        depth: int) -> tuple[float, str]:
-        """"""
+        """Metoda odpovědná za samotné vyhodnocení tahu co do bonity jeho
+        následníků (listů).
+        """
         if depth == 0 or is_terminate(board):
             return self.evaluate(board), ""
 
